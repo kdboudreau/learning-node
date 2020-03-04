a1f5659d09214ce9d10075f147cebc5fe3538ca5
Kathryn <kdboudreau1@gmail.com>
2020-03-04 16:21:55 -0500

    js expressions

--- app.js
+++ app.js
@@ -4,14 +4,23 @@ var c = a + b;
 
 console.log(c);
 
-//function statement
+// function statement
 function greet () {
     console.log('hi');
 }
 greet();
 
-//functions are first-class
+// functions are first-class
 function logGreeting(fn) {
     fn();
 }
 logGreeting(greet); //parenthesis runs the function and passes the return, no parenthesis passes the funtion
+
+// function expression
+var greetMe = function() {
+    console.log("Hey girl");
+}
+greetMe();
+
+// it's still first-class
+logGreeting(greetMe);
