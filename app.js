f1d7bd5ebef2ef39ec5785eb55a8921dc344442b
Kathryn <kdboudreau1@gmail.com>
2020-03-04 16:19:41 -0500

    js functions

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ var b = 2;
 var c = a + b;
 
 console.log(c);
+
+//function statement
+function greet () {
+    console.log('hi');
+}
+greet();
+
+//functions are first-class
+function logGreeting(fn) {
+    fn();
+}
+logGreeting(greet); //parenthesis runs the function and passes the return, no parenthesis passes the funtion
