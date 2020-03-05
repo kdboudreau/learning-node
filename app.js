ab0a5b975b9b5a25a70357dfeee651401f3cdec1
Kathryn <kdboudreau1@gmail.com>
2020-03-05 17:10:43 -0500

    another example

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ greet();
 // objects lesson
 var person = {
     firstname: 'John',
+    middleinit: 'B',
     lastname: 'Doe',
     greet: function() {
         console.log('Hello, ' + this.firstname + ' ' + this.lastname);
