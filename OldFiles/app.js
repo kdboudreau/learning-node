4dfa035dc0f8e1a9cc5f0b71e07a14f51075e268
Kathryn <kdboudreau1@gmail.com>
2020-03-12 15:43:12 -0400

    moving files to prepare for next lecture

--- OldFiles/app.js
+++ OldFiles/app.js
@@ -1,3 +1,28 @@
 var greet = require('./greet'); // no need to add .js, with no extension
     // the require funtion first looks for a file named greet.js
 greet();
+
+
+// module patterns lecture
+var greet1 = require('./greet1');
+greet1();
+
+var greet2 = require('./greet2').greet;
+greet2();
+
+var greet3 = require('./greet3');
+greet3.greet();
+greet3.greeting = "Changed hello world!";
+
+var greet3b = require('./greet3');
+greet3b.greet(); // this prints  "Changed hello world!", this is the same object as greet3
+
+var greet4 = require('./greet4');
+var grtr = new greet4(); // this now creates a new object since this is a constructor (it has the ())
+    // we aren't just using the require to create the object as we did before, which just reuses it
+    // if it has already been created and therefore cached
+grtr.greet();
+
+var greet5 = require('./greet5').greet;
+greet5();
+// here we wouldnt be able to use the greeting var from greet5 because we didnt expose it
