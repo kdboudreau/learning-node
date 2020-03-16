4913228281bf5532f57aa66da1de6465ab97b3ad
Kathryn <kdboudreau1@gmail.com>
2020-03-16 17:15:39 -0400

    requiring util

--- app.js
+++ app.js
@@ -2,4 +2,8 @@
 var greet = require('./greet');
 
 // requiring native (core) modules
-var util = require('util');
+var util = require('util'); // one clue that you're importing a core module 
+    //is if theres no slash in the require
+var name = 'Kat';
+var greeting = util.format('Hello, %s', name);
+util.log(greeting);
