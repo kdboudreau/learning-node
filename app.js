06a16b6fc14d6214bcc701e2969c1e1b4f0c501a
Kathryn <kdboudreau1@gmail.com>
2020-03-23 16:22:02 -0400

    magic string fixes

--- app.js
+++ app.js
@@ -15,3 +15,11 @@ emtr.emit('greet');
 
 // one problem with the above is that it requires magic strings
 // magic string: a string that has a special meaning in our code
+// heres a pattern to help avoid these
+var eventConfig = require('./config').events;
+
+emtr.on(eventConfig.GREET, function() {
+    console.log('Another greeting occured!');
+})
+// this way vs code can help us, and if we do get an error it will be more descriptve and helpful
+// this also allows for one place to change the var name
