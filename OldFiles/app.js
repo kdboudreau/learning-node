cc52b5b83f609cd644b5bd72e463911c2fb7e32a
Kathryn <kdboudreau1@gmail.com>
2020-03-27 16:37:08 -0400

    es6 string concat

--- OldFiles/app.js
+++ OldFiles/app.js
@@ -139,3 +139,38 @@ janey.lastname = 'Doe';
 
 console.log(johnny.greet());
 console.log(janey.greet());
+
+
+// Event emitter and inherits
+var EventEmitter = require('events');
+var util = require('util');
+
+function Greeter() {
+    this.greeting = 'Hello World!';
+}
+
+util.inherits(Greeter, EventEmitter);
+
+Greeter.prototype.greet = function() {
+    console.log(this.greeting);
+    this.emit('greet');
+}
+
+var greeter1 = new Greeter();
+greeter1.on('greet', function() {
+    console.log('Someone greeted!');
+});
+
+greeter1.greet();
+
+Greeter.prototype.greet2 = function(data) {
+    console.log(this.greeting + ': ' + data);
+    this.emit('greet', data);
+}
+
+var greeter2 = new Greeter();
+greeter2.on('greet', function(data) {
+    console.log('Someone greeted named: ' + data);
+});
+
+greeter2.greet('Kat');
--- app.js
+++ app.js
@@ -1,32 +1,8 @@
-var EventEmitter = require('events');
-var util = require('util');
+// ES6
+var name = 'John Doe';
 
-function Greeter() {
-    this.greeting = 'Hello World!';
-}
+var greet = 'Hello ' + name;
+var greet2 = `Hello ${ name }`;
 
-util.inherits(Greeter, EventEmitter);
-
-Greeter.prototype.greet = function() {
-    console.log(this.greeting);
-    this.emit('greet');
-}
-
-var greeter1 = new Greeter();
-greeter1.on('greet', function() {
-    console.log('Someone greeted!');
-});
-
-greeter1.greet();
-
-Greeter.prototype.greet2 = function(data) {
-    console.log(this.greeting + ': ' + data);
-    this.emit('greet', data);
-}
-
-var greeter2 = new Greeter();
-greeter2.on('greet', function(data) {
-    console.log('Someone greeted named: ' + data);
-});
-
-greeter2.greet('Kat');
+console.log(greet);
+console.log(greet2);
