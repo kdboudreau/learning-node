888037215a2174a4e663a7004e26fe158647e822
Kathryn <kdboudreau1@gmail.com>
2020-03-30 16:53:53 -0400

    resettign app.js

--- OldFiles/app.js
+++ OldFiles/app.js
@@ -197,3 +197,26 @@ var obj = {
 obj.greet();
 obj.greet.call({ name: 'Jane Doe' });
 obj.greet.apply({ name: 'Jane Doe' });
+
+
+// inheriting from the event emitter
+var EventEmitter = require('events');
+var util = require('util');
+
+function Greetr() {
+    EventEmitter.call(this); // the event emitter is a function constructor, so when it is invoked using
+        // the new keyword, the this var points to an empty object and it adds on properties and methods
+        // super constructor
+        // giving it the new object were working on so it adds on to this
+        // the this keyword is the same in the event emitter as it is in greetr function
+    this.greeting = 'Hello World';
+}
+
+util.inherits(Greetr, EventEmitter);
+
+Greetr.prototype.greet = function(data) {
+    console.log(this.greeting + ': ' + data);
+    this.emit('greet', data);
+}
+
+var greetr1 = new Greetr();
--- app.js
+++ app.js
@@ -1,20 +1,12 @@
-var EventEmitter = require('events');
-var util = require('util');
-
-function Greetr() {
-    EventEmitter.call(this); // the event emitter is a function constructor, so when it is invoked using
-        // the new keyword, the this var points to an empty object and it adds on properties and methods
-        // super constructor
-        // giving it the new object were working on so it adds on to this
-        // the this keyword is the same in the event emitter as it is in greetr function
-    this.greeting = 'Hello World';
+function Person (firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname;
 }
 
-util.inherits(Greetr, EventEmitter);
+var john = new Person('John', 'Doe');
+john.greet();
 
-Greetr.prototype.greet = function(data) {
-    console.log(this.greeting + ': ' + data);
-    this.emit('greet', data);
-}
+var jane = new Person('Jane', 'Doe');
+jane.greet();
 
-var greetr1 = new Greetr();
+console.log(john.__proto__);
