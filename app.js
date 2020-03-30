a7c4f15d1a63fd30438f92e66f5d7ae104d40e90
Kathryn <kdboudreau1@gmail.com>
2020-03-30 16:30:16 -0400

    event emitter 2

--- app.js
+++ app.js
@@ -1,25 +1,20 @@
-util.inherits(Greeter, EventEmitter);
+var EventEmitter = require('events');
+var util = require('util');
 
-Greeter.prototype.greet = function() {
-    console.log(this.greeting);
-    this.emit('greet');
+function Greetr() {
+    EventEmitter.call(this); // the event emitter is a function constructor, so when it is invoked using
+        // the new keyword, the this var points to an empty object and it adds on properties and methods
+        // super constructor
+        // giving it the new object were working on so it adds on to this
+        // the this keyword is the same in the event emitter as it is in greetr function
+    this.greeting = 'Hello World';
 }
 
-var greeter1 = new Greeter();
-greeter1.on('greet', function() {
-    console.log('Someone greeted!');
-});
+util.inherits(Greetr, EventEmitter);
 
-greeter1.greet();
-
-Greeter.prototype.greet2 = function(data) {
+Greetr.prototype.greet = function(data) {
     console.log(this.greeting + ': ' + data);
     this.emit('greet', data);
 }
 
-var greeter2 = new Greeter();
-greeter2.on('greet', function(data) {
-    console.log('Someone greeted named: ' + data);
-});
-
-greeter2.greet('Kat');
+var greetr1 = new Greetr();
