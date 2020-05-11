94ad99ef3ae8965de6528a0e33cf92fe666ee267
Kathryn <kdboudreau1@gmail.com>
2020-05-11 16:53:08 -0400

    writing to buff

--- app.js
+++ app.js
@@ -3,3 +3,7 @@ console.log(buff); // <Buffer 48 65 6c 6c 6f>
 console.log(buff.toString()); // Hello
 console.log(buff.toJSON()); // { type: 'Buffer', data: [ 72, 101, 108, 108, 111 ] }
 console.log(buff[2]); // 108
+
+buff.write('wo');
+console.log(buff.toString()); // wollo
+//buffers are meant to be a finite piece of data. here, 5 characters
