f90ef117362514707fb625e2c9322ffa81a9c499
Kathryn <kdboudreau1@gmail.com>
2020-05-13 16:15:22 -0400

    reading txt file

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 var fs = require('fs');
+
+var greetFS = fs.readFileSync(__dirname + '/greet.txt', 'utf8'); // sync = synchronous
+    // __dirname gives path to directory. also, 'utf8' is default encoding
+
+console.log(greetFS); // Hello from file World
+
+// typically dont want this to be synchronous, can take a long time if there are a lot of users trying
+    // to access a large file
+    // below function takes care of this 
+
+var greetFS2 = fs.readFile(__dirname + '/greet.txt', 
+    function(err, data) {
+    
+    });
+    // this is a popular pattern in node, "error-first callback" 
