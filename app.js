e3bf82787053edc187f3f4501c6ab6064d66a8fb
Kathryn <kdboudreau1@gmail.com>
2020-06-03 17:02:16 -0400

    stream functions

--- app.js
+++ app.js
@@ -4,3 +4,13 @@
 // duplex (read and write) and transform (change data as it moves through)
 
 // abstract (base) class: a type of constructor you never work directly with, but inherit from
+
+var fs = require('fs');
+
+var readable = fs.createReadStream(__dirname + '/greetLoremIpsum.txt');
+
+readable.on('data', function(chunk) {
+    console.log(chunk);
+    // <Buffer 4c 6f 72 65 6d 20 69 70 73 75 6d 20 64 6f 6c 6f 72 20 73 69 74 20 61 6d 65 74 2c 20 63 6f 6e 73 65 63 74 65 74 75 72 20 61 64 69 70
+    // 69 73 63 69 6e 67 ... 978 more bytes>
+});
