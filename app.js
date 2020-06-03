62495ef9a501081cde4fddb54b068fc6db350840
Kathryn <kdboudreau1@gmail.com>
2020-06-03 16:54:56 -0400

    morem definitions

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 // stream: a sequence of pieces of data
 // streams are event emitters. they have access to on and emit
+// there are now readable (read data) and writable (can only send, nto read) streams,
+// duplex (read and write) and transform (change data as it moves through)
+
+// abstract (base) class: a type of constructor you never work directly with, but inherit from
