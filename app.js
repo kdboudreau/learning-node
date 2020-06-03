ae287127d5bd3d6a019baf734164258acbafa21b
Kathryn <kdboudreau1@gmail.com>
2020-06-03 17:04:16 -0400

    stream functions

--- app.js
+++ app.js
@@ -7,10 +7,12 @@
 
 var fs = require('fs');
 
-var readable = fs.createReadStream(__dirname + '/greetLoremIpsum.txt');
+var readable = fs.createReadStream(__dirname + '/greetLoremIpsum.txt',
+    { encoding: 'utf8'});
 
 readable.on('data', function(chunk) {
     console.log(chunk);
-    // <Buffer 4c 6f 72 65 6d 20 69 70 73 75 6d 20 64 6f 6c 6f 72 20 73 69 74 20 61 6d 65 74 2c 20 63 6f 6e 73 65 63 74 65 74 75 72 20 61 64 69 70
+    // with no options you get this: <Buffer 4c 6f 72 65 6d 20 69 70 73 75 6d 20 64 6f 6c 6f 72 20 73 69 74 20 61 6d 65 74 2c 20 63 6f 6e 73 65 63 74 65 74 75 72 20 61 64 69 70
     // 69 73 63 69 6e 67 ... 978 more bytes>
+    // with encoding : utf8 you get the text
 });
