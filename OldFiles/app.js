85035b25f70428f72a8f4cb613b5ab6a7c935e03
Kathryn <kdboudreau1@gmail.com>
2020-06-05 14:56:57 -0400

    resetting files

--- OldFiles/app.js
+++ OldFiles/app.js
@@ -337,3 +337,28 @@ console.log('Done!');
 // Done!
 // <Buffer 48 65 6c 6c 6f 20 66 72 6f 6d 20 66 69 6c 65 20 57 6f 72 6c 64>
 // Hello from file World
+
+
+
+// stream: a sequence of pieces of data
+// streams are event emitters. they have access to on and emit
+// there are now readable (read data) and writable (can only send, nto read) streams,
+// duplex (read and write) and transform (change data as it moves through)
+
+// abstract (base) class: a type of constructor you never work directly with, but inherit from
+
+var fs = require('fs');
+
+var readable = fs.createReadStream(__dirname + '/greetLoremIpsum.txt',
+    { encoding: 'utf8', highwatermark: 16 * 1024 });
+
+var writable = fs.createWriteStream(__dirname + '/greetLoremIpsumCopy.txt');
+
+readable.on('data', function(chunk) {
+    console.log(chunk);
+    // with no options you get this: <Buffer 4c 6f 72 65 6d 20 69 70 73 75 6d 20 64 6f 6c 6f 72 20 73 69 74 20 61 6d 65 74 2c 20 63 6f 6e 73 65 63 74 65 74 75 72 20 61 64 69 70
+    // 69 73 63 69 6e 67 ... 978 more bytes>
+    // with encoding : utf8 you get the text
+    // with highwatermark you indicate how big you want the buffer to be. 4 chunks here
+    writable.write(chunk);
+}); //this method is so common theres a better way to do this in node
