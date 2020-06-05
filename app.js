ed1d81eeaf0de9326a1fa92255b79009e5439f24
Kathryn <kdboudreau1@gmail.com>
2020-06-05 15:18:46 -0400

    using pipes

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 // pipe: connecting two streams by writing to one stream what is being read from another
 // in node you pipe from a readable stream to a writable
+
+var fs = require('fs');
+
+var readable = fs.createReadStream(__dirname + '/greetLoremIpsum.txt',
+    { encoding: 'utf8', highwatermark: 16 * 1024 });
+
+var writable = fs.createWriteStream(__dirname + '/greetLoremIpsumCopy.txt');
+
+readable.pipe(writable);
