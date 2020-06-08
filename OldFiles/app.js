03ee2ae9fcd35848866e53f1968807a5703e3a5f
Kathryn <kdboudreau1@gmail.com>
2020-06-08 15:24:17 -0400

    cleaning files

--- OldFiles/app.js
+++ OldFiles/app.js
@@ -362,3 +362,19 @@ readable.on('data', function(chunk) {
     // with highwatermark you indicate how big you want the buffer to be. 4 chunks here
     writable.write(chunk);
 }); //this method is so common theres a better way to do this in node
+
+
+// pipe: connecting two streams by writing to one stream what is being read from another
+// in node you pipe from a readable stream to a writable
+
+var fsPipe = require('fs');
+var zlib = require('zlib');
+
+var readable2 = fsPipe.createReadStream(__dirname + '/greetLoremIpsum.txt',
+    { encoding: 'utf8', highwatermark: 16 * 1024 });
+var writable2 = fs.createWriteStream(__dirname + '/greetLoremIpsumCopy.txt');
+var compressed = fsPipe.createWriteStream(__dirname + '/greetLoremIpsum.txt.gz');
+var gzip = zlib.createGzip();
+
+readable2.pipe(writable2); //updates copy file
+readable2.pipe(gzip).pipe(compressed);
