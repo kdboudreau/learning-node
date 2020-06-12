6de1de591c22c203fe466e3320312749f6db76ba
Kathryn <kdboudreau1@gmail.com>
2020-06-12 15:47:27 -0400

    create a webserver

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 
-http.createServer(function() {
-    
-})
+http.createServer(function(req, resp) { // request and response
+    resp.writeHead(200, { 'Content-Type' : 'text/plain' }); // 200 response code, header
+    resp.end('Hello World\n'); // \n is a carriage return
+}).listen(1337, '127.0.0.1'); // port, address (this example is the standard local host)
