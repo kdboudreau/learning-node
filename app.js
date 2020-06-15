c2db6e1e7ad4bb21d17e51ed078b5fcfbb58dd28
Kathryn <kdboudreau1@gmail.com>
2020-06-15 13:48:28 -0400

    more server notes

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ http.createServer(function(req, resp) { // request and response
     resp.end('Hello World\n'); // \n is a carriage return
 }).listen(1337, '127.0.0.1'); // port, address (this example is the standard local host)
 
-//running node app.js will keep running, it waits for a request
+// running node app.js will keep running, it waits for a request. can stop server by ctrl + c
 // when we hit the server using url localhost:1337 and hit enter, the page will show "hello world"
 // also, if you inspect you will see responsecode of 200 and content-type of text/plain
