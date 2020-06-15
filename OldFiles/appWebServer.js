c1c9edeb734241b73379e77760855aac6f14325f
Kathryn <kdboudreau1@gmail.com>
2020-06-15 14:00:46 -0400

    using html as message

--- OldFiles/appWebServer.js
+++ OldFiles/appWebServer.js
@@ -24,3 +24,14 @@ import { request } from "http"
 
 // with all of this, we have everything we need to create a server: connect to the internet, 
 //       send a request, receive a response
+
+var http = require('http');
+
+http.createServer(function(req, resp) { // request and response
+    resp.writeHead(200, { 'Content-Type' : 'text/plain' }); // 200 response code, header
+    resp.end('Hello World\n'); // \n is a carriage return
+}).listen(1337, '127.0.0.1'); // port, address (this example is the standard local host)
+
+// running node app.js will keep running, it waits for a request. can stop server by ctrl + c
+// when we hit the server using url localhost:1337 and hit enter, the page will show "hello world"
+// also, if you inspect you will see responsecode of 200 and content-type of text/plain
--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 var http = require('http');
+var fs = require('fs');
 
-http.createServer(function(req, resp) { // request and response
-    resp.writeHead(200, { 'Content-Type' : 'text/plain' }); // 200 response code, header
-    resp.end('Hello World\n'); // \n is a carriage return
-}).listen(1337, '127.0.0.1'); // port, address (this example is the standard local host)
-
-// running node app.js will keep running, it waits for a request. can stop server by ctrl + c
-// when we hit the server using url localhost:1337 and hit enter, the page will show "hello world"
-// also, if you inspect you will see responsecode of 200 and content-type of text/plain
+http.createServer(function(req, resp) {
+    resp.writeHead(200, { 'Content-Type' : 'text/html' });
+    var html = fs.readFileSync(__dirname + '/index.htm');
+    resp.end(html);
+}).listen(1337, '127.0.0.1'); 
