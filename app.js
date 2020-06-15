2caccc4fad6cbfe34c4267b67b01b2af97632d5d
Kathryn <kdboudreau1@gmail.com>
2020-06-15 14:31:39 -0400

    using templates

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var fs = require('fs');
 
 http.createServer(function(req, resp) {
     resp.writeHead(200, { 'Content-Type' : 'text/html' });
-    var html = fs.readFileSync(__dirname + '/index.htm');
+    var html = fs.readFileSync(__dirname + '/index.htm', 'utf8'); //utf8 converts it to string
+    var message = 'Hello world...';
+    html = html.replace('{Message}', message);
     resp.end(html);
 }).listen(1337, '127.0.0.1'); 
