f5a2cef20791fda2d8ce9cf5f2a9ab925cddaa2d
Kathryn <kdboudreau1@gmail.com>
2020-06-17 15:13:29 -0400

    resetting

--- OldFiles/appWebServer.js
+++ OldFiles/appWebServer.js
@@ -58,3 +58,18 @@ http.createServer(function(req, resp) {
     resp.writeHead(200, { 'Content-Type' : 'text/html' });
     fs.createReadStream(__dirname + '/index.htm').pipe(resp); 
 }).listen(1337, '127.0.0.1'); 
+
+
+// using json
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, resp) {
+    resp.writeHead(200, { 'Content-Type' : 'application/json' });
+    var obj = {
+        firstname: 'John',
+        lastname: 'Doe'
+    };
+    resp.end(JSON.stringify(obj));
+}).listen(1337, '127.0.0.1'); 
+// outputs {"firstname":"John","lastname":"Doe"}
--- app.js
+++ app.js
@@ -1,13 +1,1 @@
-// using json
-var http = require('http');
-var fs = require('fs');
 
-http.createServer(function(req, resp) {
-    resp.writeHead(200, { 'Content-Type' : 'application/json' });
-    var obj = {
-        firstname: 'John',
-        lastname: 'Doe'
-    };
-    resp.end(JSON.stringify(obj));
-}).listen(1337, '127.0.0.1'); 
-// outputs {"firstname":"John","lastname":"Doe"}
