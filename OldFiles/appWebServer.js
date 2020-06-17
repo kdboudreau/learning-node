3f8a886c749b1500cfb26ff13a64a8d33ca9fcf3
Kathryn <kdboudreau1@gmail.com>
2020-06-17 14:54:41 -0400

    resetting files

--- OldFiles/appWebServer.js
+++ OldFiles/appWebServer.js
@@ -48,3 +48,13 @@ http.createServer(function(req, resp) {
     html = html.replace('{Message}', message);
     resp.end(html);
 }).listen(1337, '127.0.0.1'); 
+
+
+// using streams
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, resp) {
+    resp.writeHead(200, { 'Content-Type' : 'text/html' });
+    fs.createReadStream(__dirname + '/index.htm').pipe(resp); 
+}).listen(1337, '127.0.0.1'); 
--- app.js
+++ app.js
@@ -1,8 +1,2 @@
-// using streams
-var http = require('http');
-var fs = require('fs');
 
-http.createServer(function(req, resp) {
-    resp.writeHead(200, { 'Content-Type' : 'text/html' });
-    fs.createReadStream(__dirname + '/index.htm').pipe(resp); 
-}).listen(1337, '127.0.0.1'); 
+
