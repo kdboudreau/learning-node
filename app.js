04f096049116573c0ee9018d608774422313cbc7
Kathryn <kdboudreau1@gmail.com>
2020-06-17 14:59:27 -0400

    apis 2

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+// API: a set of tools for building a software application; application programming interface
+// on the webthe tools are usually made available via a set of urls which accept and send only 
+    // data via http and tcp/ip 
+
+// endpoint: one url in a web api
 
 
