084ecfdcf760503bce7c6c9069714c459358ce2c
Kathryn <kdboudreau1@gmail.com>
2020-06-23 17:11:22 -0400

    updating notes

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 // --save: npm will download the moment files, and save a reference in the package.json
+//  "moment": "^2.27.0" the carat ^ means that its ok if npm auto updates within the major release
+    // like 2.27.1 or 2.27, just not 3.0.0
+    
