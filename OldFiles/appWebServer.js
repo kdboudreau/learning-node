6cd368f98a9efe11f086a95515b8d0bf2e8b8d7a
Kathryn <kdboudreau1@gmail.com>
2020-06-23 16:51:06 -0400

    resetting files

--- OldFiles/appWebServer.js
+++ OldFiles/appWebServer.js
@@ -97,3 +97,10 @@ http.createServer(function(req, resp) {
         resp.end();
     }
 }).listen(1337, '127.0.0.1'); 
+
+
+// package: code, managed and maintained
+// package management system: software that automate installing and updating packages
+// npm can be 2 things: 
+    // 1. the registry, the packages themselves
+    // 2. the program thats installed, the package management system
--- app.js
+++
@@ -1,5 +0,0 @@
-// package: code, managed and maintained
-// package management system: software that automate installing and updating packages
-// npm can be 2 things: 
-    // 1. the registry, the packages themselves
-    // 2. the program thats installed, the package management system
