0c93be2537e8a09b42dcc8138d15f116b9c6e08f
Kathryn <kdboudreau1@gmail.com>
2020-06-24 16:18:14 -0400

    nodemon

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // --save: npm will download the moment files, and save a reference in the package.json
 // --save-dev will add to devDependencies in package.json. not needed to run app, but useful for devs
+    // the server wont install devdependencies when its pushed up
+// -g means globally install
+    // dont install to node_modules, put it in a location that is accessable to all apps
 //  "moment": "^2.27.0" the carat ^ means that its ok if npm auto updates within the major release
     // like 2.27.1 or 2.27, just not 3.0.0
 
@@ -8,3 +11,6 @@
 
 var moment = require('moment');
 console.log(moment().format("ddd, hA"));
+
+// nodemon: aka node monitor; this monitors files, so if we chnge a file it automatically stops and
+    // restarts node app.js
