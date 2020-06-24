52e6e7542d462c470f7563a2d7dcf89c76062758
Kathryn <kdboudreau1@gmail.com>
2020-06-24 16:09:14 -0400

    pdating package.json with jasmine dev dependency

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // --save: npm will download the moment files, and save a reference in the package.json
+// --save-dev will add to devDependencies in package.json. not needed to run app, but useful for devs
 //  "moment": "^2.27.0" the carat ^ means that its ok if npm auto updates within the major release
     // like 2.27.1 or 2.27, just not 3.0.0
 
