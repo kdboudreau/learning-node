fa2a378e40ad1a5e2fda3e8fad30f5545eef9b64
Kathryn <kdboudreau1@gmail.com>
2020-06-25 14:27:55 -0400

    using assets

--- app.js
+++ app.js
@@ -5,12 +5,19 @@ var port = process.env.PORT || 3000;
     // could use this if prod server has a port defined, that way we use either the port specified
     // by the server, of 3000 locally
 
+app.use('/assets', express.static(__dirname + '/public')); 
+    //anything in the public folder is available by using assets
+
 app.get('/', function(req, resp) {
-    resp.send('<html><head></head><body><h1>Hello world!</h1></body></html>');
+    resp.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet/></head><body><h1>Hello world!</h1></body></html>');
 });
 
 app.get('/api', function(req, resp) {
     resp.json({ firstname : 'John', lastname: 'Doe' });
+});
+
+app.get('/person/:id', function(req, resp) {
+    resp.send('<html><head></head><body><h1>Person: ' + req.params.id + '</h1></body></html>');
 });
 
 app.listen('3000'); // this creates the server
